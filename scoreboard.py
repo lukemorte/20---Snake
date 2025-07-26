efccaf4ed2538a036c68cca217a18c9c14ead50d
Jakub Lukáš <jakublukas.eclairdesign@gmail.com>
2025-07-26 11:09:09 +0200

    Fix high score initialization and implement saving functionality in Scoreboard

--- scoreboard.py
+++ scoreboard.py
@@ -7,7 +7,7 @@ class Scoreboard(Turtle):
     def __init__(self):
         super().__init__()
         self.score = 0
-        self.high_score = 0
+        self.high_score = self.get_highscore_from_file()
         self.color("white")
         self.penup()
         self.hideturtle()
@@ -19,15 +19,25 @@ class Scoreboard(Turtle):
         self.write(f"score: {self.score} | high score: {self.high_score}", False, align=ALIGNMENT, font=FONT)
 
     def increase_score(self):
-        self.score += 1        
+        self.score += 1
         self.update_scoreboard()
 
     def reset(self):
         if self.score > self.high_score:
             self.high_score = self.score
+            self.save_highsore_to_file(self.high_score)
         self.score = 0
         self.update_scoreboard()
 
+    def get_highscore_from_file(self):
+        with open("data.txt", "r") as file:
+            high_score = file.read()
+            return int(high_score)
+
+    def save_highsore_to_file(self, p_highscore):
+        with open("data.txt", "w") as file:
+            file.write(str(p_highscore))
+
     # def game_over(self):
-    #     self.goto(0, 0)
+    #     self.goto(0, 0)\
     #     self.write("GAME OVER", False, align=ALIGNMENT, font=FONT_GAME_OVER)
